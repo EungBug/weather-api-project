8d437d8dca83e4e0997f1a42ef2ae4c55e53bed0
EungBug <eungb14@gmail.com>
2023-04-14 01:06:46 +0900

    날짜 정보 구현

--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ const BASE_URL = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0';
 const GET_ULTRA_SRT_NCST = '/getUltraSrtNcst';
 const SERVICE_KEY = '7RzOAYDkB9qRHVsXHVLPuEAUsikSSpD4YqMjJ47VbykQRu+GF6nvvmzo6K72vQ3aIJBHN/1p2uSVEhJm7B01BA==';
 
+// 격자 위경도 (기본값 사직동)
+let nx = 60;
+let ny = 127;
+
+// moment.js 로케일 설정
+moment.locale('en');
+
 // 현재 사용자 위치 값 받아오기
 function getUserLoaction() {
   return new Promise(resolve => {
@@ -13,15 +20,21 @@ function getUserLoaction() {
       navigator.geolocation.getCurrentPosition(position => {
         console.log(position.coords.latitude);
         console.log(position.coords.longitude);
-        resolve(position);
+        const convertXY = dfs_xy_conv('toXY', `${position.coords.latitude}`, `${position.coords.longitude}`);
+        nx = convertXY.x;
+        ny = convertXY.y;
+        resolve();
       });
+    } else {
+      // 기본 위치 값으로 불러온다.
+      resolve();
     }
   });
 }
 
 (() => {
-  getUserLoaction().then(position => {
-    console.log(`done ${position}`);
+  getUserLoaction().then(() => {
+    getWetherInfo();
   });
 })();
 
@@ -35,17 +48,38 @@ async function getWetherInfo() {
       pageNo: '1',
       numOfRow: '1000',
       dataType: 'JSON',
-      base_date: '20230413',
-      base_time: '0600',
-      nx: '52',
-      ny: '127'
+      base_date: getToday(),
+      base_time: getTime(),
+      nx: nx,
+      ny: ny
     }
   });
 
   console.log(res.data);
 }
 
-(async () => getWetherInfo())();
+function getToday() {
+  const today = new Date();
+  const month = today.getMonth() + 1 < 9 ? `0${today.getMonth() + 1}` : String(today.getMonth() + 1);
+  const day = today.getDate() < 9 ? `0${today.getDate()}` : String(today.getDate());
+
+  return `${today.getFullYear()}${month}${day}`;
+}
+
+function getTime() {
+  const today = new Date();
+  const hours = today.getHours() < 9 ? `0${today.getHours()}` : String(today.getHours());
+  const minutes = today.getMinutes() < 9 ? `0${today.getMinutes()}` : String(today.getMinutes());
+  return hours + minutes;
+}
+
+function initTodayUI() {
+  const timeEl = document.querySelector('p.time');
+
+  timeEl.textContent = moment().format('LT');
+}
+
+initTodayUI();
 
 /*
   // queryString
